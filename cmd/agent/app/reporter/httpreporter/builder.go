538fa90d6573b773d06701905451ab80200bb547
apoorv <apoorv@archsaber.com>
2018-05-05 18:41:02 +0530

    concurrency fixes

--- cmd/agent/app/reporter/httpreporter/builder.go
+++ cmd/agent/app/reporter/httpreporter/builder.go
@@ -1,9 +1,13 @@
 package httpreporter
 
 import (
+	"context"
 	"errors"
+	"io/ioutil"
 
 	"github.com/apache/thrift/lib/go/thrift"
+	"github.com/jaegertracing/jaeger/thrift-gen/jaeger"
+	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
 	"github.com/uber/jaeger-lib/metrics"
 	"go.uber.org/zap"
 )
@@ -13,6 +17,8 @@ type Builder struct {
 	// CollectorHostPort are host:ports of a static list of Jaeger Collectors.
 	CollectorHostPorts []string `yaml:"collectorHTTPHostPorts"`
 	TokenFile          string   `yaml:"tokenFile"`
+
+	transport thrift.TTransport
 }
 
 // NewBuilder creates a new reporter builder.
@@ -22,13 +28,52 @@ func NewBuilder() *Builder {
 
 // CreateReporter creates the HTTP-based Reporter
 func (b *Builder) CreateReporter(mFactory metrics.Factory, logger *zap.Logger) (*Reporter, error) {
+	trans, err := b.getNewTTransport()
+	if err != nil {
+		return nil, err
+	}
+	protFactory := b.getNewTProtocolFactory()
+	r := &Reporter{
+		jClient: jaeger.NewCollectorClientFactory(trans, protFactory),
+		zClient: zipkincore.NewZipkinCollectorClientFactory(trans, protFactory),
+		logger:  logger,
+		builder: b,
+	}
+	go r.watchTokenUpdates(context.Background())
+	return r, nil
+}
+
+func (b *Builder) getNewTProtocolFactory() thrift.TProtocolFactory {
+	return thrift.NewTBinaryProtocolFactoryDefault()
+}
+
+func (b *Builder) getNewTTransport() (thrift.TTransport, error) {
+	if b.transport != nil {
+		b.transport.Close()
+		b.transport = nil
+	}
+	token := readTokenFromFile(b.TokenFile)
 	if len(b.CollectorHostPorts) == 0 {
 		return nil, errors.New("collector address string not specified")
 	}
+
 	// connect to the first collector for now
 	transport, err := thrift.NewTHttpPostClient("https://" + b.CollectorHostPorts[0])
 	if err != nil {
 		return nil, err
 	}
-	return newReporter(transport, thrift.NewTBinaryProtocolFactoryDefault(), b.TokenFile, logger), nil
+	httpTransport, _ := (transport).(*thrift.THttpClient)
+	if token != "" {
+		httpTransport.SetHeader("Authorization", token)
+	}
+	b.transport = httpTransport
+	return httpTransport, nil
+}
+
+func readTokenFromFile(tokenFile string) string {
+	tokenBytes, err := ioutil.ReadFile(tokenFile)
+	if err != nil {
+		return ""
+	}
+	return string(tokenBytes)
 }
--- cmd/agent/app/reporter/httpreporter/reporter.go
+++ cmd/agent/app/reporter/httpreporter/reporter.go
@@ -2,10 +2,8 @@ package httpreporter
 
 import (
 	"context"
-	"io/ioutil"
 	"sync"
 
-	"github.com/apache/thrift/lib/go/thrift"
 	"github.com/fsnotify/fsnotify"
 	"github.com/jaegertracing/jaeger/thrift-gen/jaeger"
 	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
@@ -14,40 +12,19 @@ import (
 
 // Reporter forwards received spans to central collector tier over HTTP.
 type Reporter struct {
-	thttpClient *thrift.THttpClient
-
 	jClient *jaeger.CollectorClient
 	zClient *zipkincore.ZipkinCollectorClient
 	logger  *zap.Logger
 
-	// authorization token
-	tokenFile string
-	token     string
+	// builder for this reporter
+	builder *Builder
 
-	// sync mechanism for token access
+	// sync mechanism for jClient and zClient access
 	rw sync.RWMutex
 }
 
-func (r *Reporter) getToken() string {
-	r.rw.RLock()
-	token := r.token
-	r.rw.RUnlock()
-	return token
-}
-
-func (r *Reporter) updateToken() {
-	tokenBytes, err := ioutil.ReadFile(r.tokenFile)
-	if err != nil {
-		r.logger.Error("unable to set token from file")
-		return
-	}
-	r.rw.Lock()
-	r.token = string(tokenBytes)
-	r.rw.Unlock()
-}
-
 func (r *Reporter) watchTokenUpdates(ctx context.Context) {
-	r.logger.Info("watching updates on " + r.tokenFile)
+	r.logger.Info("watching updates on " + r.builder.TokenFile)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		r.logger.Fatal("unable to watch for token updates")
@@ -55,7 +32,7 @@ func (r *Reporter) watchTokenUpdates(ctx context.Context) {
 	}
 	defer watcher.Close()
 
-	err = watcher.Add(r.tokenFile)
+	err = watcher.Add(r.builder.TokenFile)
 	if err != nil {
 		r.logger.Fatal(err.Error())
 		return
@@ -66,7 +43,7 @@ func (r *Reporter) watchTokenUpdates(ctx context.Context) {
 		case event := <-watcher.Events:
 			if event.Op&fsnotify.Write == fsnotify.Write ||
 				event.Op&fsnotify.Create == fsnotify.Create {
-				r.updateToken()
+				r.updateClients()
 			}
 		case err := <-watcher.Errors:
 			r.logger.Error(err.Error())
@@ -76,33 +53,31 @@ func (r *Reporter) watchTokenUpdates(ctx context.Context) {
 	}
 }
 
-// New returns a new Reporter
-func newReporter(trans thrift.TTransport, protFactory thrift.TProtocolFactory, tokenFile string,
-	zlogger *zap.Logger) *Reporter {
-
-	httpTransport, _ := (trans).(*thrift.THttpClient)
-	r := &Reporter{
-		thttpClient: httpTransport,
-		jClient:     jaeger.NewCollectorClientFactory(httpTransport, protFactory),
-		zClient:     zipkincore.NewZipkinCollectorClientFactory(httpTransport, protFactory),
-		tokenFile:   tokenFile,
-		logger:      zlogger,
+func (r *Reporter) updateClients() {
+	protFactory := r.builder.getNewTProtocolFactory()
+	trans, err := r.builder.getNewTTransport()
+	if err != nil {
+		r.logger.Error(err.Error())
+		return
 	}
-	r.updateToken()
-	go r.watchTokenUpdates(context.Background())
-	return r
+	r.rw.Lock()
+	r.jClient = jaeger.NewCollectorClientFactory(trans, protFactory)
+	r.zClient = zipkincore.NewZipkinCollectorClientFactory(trans, protFactory)
+	r.rw.Unlock()
 }
 
 // EmitZipkinBatch implements EmitZipkinBatch() of Reporter
 func (r *Reporter) EmitZipkinBatch(spans []*zipkincore.Span) error {
-	r.thttpClient.SetHeader("Authorization", r.getToken())
+	r.rw.RLock()
 	_, err := r.zClient.SubmitZipkinBatch(spans)
+	r.rw.RUnlock()
 	return err
 }
 
 // EmitBatch implements EmitBatch() of Reporter
 func (r *Reporter) EmitBatch(batch *jaeger.Batch) error {
-	r.thttpClient.SetHeader("Authorization", r.getToken())
+	r.rw.RLock()
 	_, err := r.jClient.SubmitBatches([]*jaeger.Batch{batch})
+	r.rw.RUnlock()
 	return err
 }
